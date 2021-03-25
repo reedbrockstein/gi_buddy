228af0d541bbdfbef0d4346d7051a079d8752ddd
rbroc <reedlb2@illinois.edu>
2021-03-25 18:08:51 -0500

    Initial commit

---
+++ lib/MeasureWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class MeasureWidget extends StatelessWidget {
+  final Color color;
+
+  MeasureWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      color: color,
+      child: Text('cfghjkjhgfghjk'),
+    );
+  }
+}
